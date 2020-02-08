09c84a8621421ca203cb35d783bfaa38ef168448
root <imjamesyoung@gmail.com>
2020-02-08 03:49:31 +0000

    show club css, comments, responsive img

---
+++ seed.js
@@ -0,0 +1,65 @@
+var mongoose = require("mongoose");
+var Club = require("./models/club");
+var Comment = require("./models/comment");
+ 
+var data = [
+    {
+        name: "SFU Hikers", 
+        image: "https://go.sfss.ca/clubs/622/logo",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "SFU AI", 
+        image: "https://go.sfss.ca/clubs/824/logo",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Altered Reality", 
+        image: "https://go.sfss.ca/clubs/56/logo",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    }
+]
+ 
+function seedDB(){
+   //Remove all clubs
+	console.log("seed")
+   Club.remove({}, function(err){
+        if(err){
+            console.log(err);
+        }
+        console.log("removed campgrounds!");
+        Comment.remove({}, function(err) {
+            if(err){
+                console.log(err);
+            }
+            console.log("removed comments!");
+             //add a few clubs
+            data.forEach(function(seed){
+                Club.create(seed, function(err, clubs){
+                    if(err){
+                        console.log(err)
+                    } else {
+                        console.log("added a campground");
+                        //create a comment
+                        Comment.create(
+                            {
+                                text: "This place is great, but I wish there was internet",
+                                author: "Homer"
+                            }, function(err, comment){
+                                if(err){
+                                    console.log(err);
+                                } else {
+                                    clubs.comments.push(comment);
+                                    clubs.save();
+                                    console.log("Created new comment");
+                                }
+                            });
+                    }
+                });
+            });
+        });
+    }); 
+    //add a few comments
+}
+ 
+module.exports = seedDB;
